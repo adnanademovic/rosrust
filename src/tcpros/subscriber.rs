41ec153d90748570fa864b5580a33cbf96817410
Adnan Ademovic <adnanademovic100@gmail.com>
2016-09-26 10:41:53 +0200

    Use iterator interface for TCPROS subscriber

--- src/tcpros/subscriber.rs
+++ src/tcpros/subscriber.rs
@@ -2,41 +2,34 @@ use byteorder::{LittleEndian, ReadBytesExt};
 use rustc_serialize::Decodable;
 use std::net::{TcpStream, ToSocketAddrs};
 use std::io::{Read, Write};
+use std::sync::mpsc;
+use std::thread;
 use std;
 use super::error::Error;
 use super::header::{encode, decode};
 use super::message::RosMessage;
 use super::decoder::Decoder;
 
-pub trait SubscriberCallback {
-    type N: RosMessage + Decodable;
-
-    fn receive(&mut self, message: Self::N);
-}
-
-pub struct Subscriber<T: SubscriberCallback> {
-    stream: Decoder<std::io::Bytes<TcpStream>>,
-    callback: T,
+pub struct Subscriber<T>
+    where T: RosMessage + Decodable + Send + 'static
+{
+    rx: mpsc::Receiver<T>,
 }
 
 impl<T> Subscriber<T>
-    where T: SubscriberCallback
+    where T: RosMessage + Decodable + Send + 'static
 {
-    pub fn new<U>(address: U,
-                  callback: T,
-                  caller_id: &str,
-                  topic: &str)
-                  -> Result<Subscriber<T>, Error>
+    pub fn new<U>(address: U, caller_id: &str, topic: &str) -> Result<Subscriber<T>, Error>
         where U: ToSocketAddrs
     {
         let mut stream = try!(TcpStream::connect(address));
         {
             let mut fields = std::collections::HashMap::<String, String>::new();
-            fields.insert("message_definition".to_owned(), T::N::msg_definition());
+            fields.insert("message_definition".to_owned(), T::msg_definition());
             fields.insert("callerid".to_owned(), caller_id.to_owned());
             fields.insert("topic".to_owned(), topic.to_owned());
-            fields.insert("md5sum".to_owned(), T::N::md5sum());
-            fields.insert("type".to_owned(), T::N::msg_type());
+            fields.insert("md5sum".to_owned(), T::md5sum());
+            fields.insert("type".to_owned(), T::msg_type());
 
             let fields = try!(encode(fields));
 
@@ -51,23 +44,37 @@ impl<T> Subscriber<T>
             try!(stream.read_exact(&mut payload));
             let data = bytes.iter().chain(payload.iter()).cloned().collect();
             let fields = try!(decode(data));
-            if fields.get("md5sum") != Some(&T::N::md5sum()) {
+            if fields.get("md5sum") != Some(&T::md5sum()) {
                 return Err(Error::Mismatch);
             }
-            if fields.get("type") != Some(&T::N::msg_type()) {
+            if fields.get("type") != Some(&T::msg_type()) {
                 return Err(Error::Mismatch);
             }
         }
 
-        Ok(Subscriber {
-            stream: Decoder::new(stream.bytes()),
-            callback: callback,
-        })
+        let (tx, rx) = mpsc::channel();
+
+        thread::spawn(move || spin_subscriber(stream, tx));
+
+        Ok(Subscriber { rx: rx })
     }
+}
 
-    pub fn spin(&mut self) -> Result<(), Error> {
-        loop {
-            self.callback.receive(try!(T::N::decode(&mut self.stream)));
-        }
+fn spin_subscriber<T>(stream: TcpStream, tx: mpsc::Sender<T>) -> Result<(), Error>
+    where T: Decodable
+{
+    let mut stream = Decoder::new(stream.bytes());
+    while let Ok(()) = tx.send(try!(T::decode(&mut stream))) {
+    }
+    Ok(())
+}
+
+impl<T> std::iter::Iterator for Subscriber<T>
+    where T: RosMessage + Decodable + Send + 'static
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.rx.recv().ok()
     }
 }
