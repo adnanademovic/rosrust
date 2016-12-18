7bab9c248f829d7f139f827e312c3aa25c827fd6
Adnan Ademovic <adnanademovic100@gmail.com>
2016-12-18 01:59:11 +0100

    Add tests to TCPROS encoder

--- src/tcpros/encoder.rs
+++ src/tcpros/encoder.rs
@@ -17,8 +17,8 @@ impl Encoder {
         self.output.iter().fold(0, |accum, ref v| accum + v.len())
     }
 
-    pub fn write_to<T: std::io::Write>(self, output: &mut T) -> Result<(), std::io::Error> {
-        for v in self.output {
+    pub fn write_to<T: std::io::Write>(&self, output: &mut T) -> Result<(), std::io::Error> {
+        for ref v in &self.output {
             output.write_all(&v)?;
         }
         Ok(())
@@ -261,5 +261,209 @@ impl rustc_serialize::Encoder for Encoder {
         where F: FnOnce(&mut Self) -> Result<(), Self::Error>
     {
         Err(Error::UnsupportedData)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std;
+    use rustc_serialize::Encodable;
+
+    fn pull_data(encoder: &Encoder) -> Vec<u8> {
+        let mut cursor = std::io::Cursor::new(Vec::new());
+        encoder.write_to(&mut cursor).unwrap();
+        cursor.into_inner()
+    }
+
+    #[test]
+    fn starts_empty() {
+        assert_eq!(0, Encoder::new().len());
+    }
+
+    #[test]
+    fn writes_u8() {
+        let mut encoder = Encoder::new();
+        150u8.encode(&mut encoder).unwrap();
+        assert_eq!(vec![1, 0, 0, 0, 150], pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_u16() {
+        let mut encoder = Encoder::new();
+        0xA234u16.encode(&mut encoder).unwrap();
+        assert_eq!(vec![2, 0, 0, 0, 0x34, 0xA2], pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_u32() {
+        let mut encoder = Encoder::new();
+        0xCD012345u32.encode(&mut encoder).unwrap();
+        assert_eq!(vec![4, 0, 0, 0, 0x45, 0x23, 1, 0xCD], pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_u64() {
+        let mut encoder = Encoder::new();
+        0xAB9876543210AABBu64.encode(&mut encoder).unwrap();
+        assert_eq!(vec![8, 0, 0, 0, 0xBB, 0xAA, 0x10, 0x32, 0x54, 0x76, 0x98, 0xAB],
+                   pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_i8() {
+        let mut encoder = Encoder::new();
+        (-100i8).encode(&mut encoder).unwrap();
+        assert_eq!(vec![1, 0, 0, 0, 156], pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_i16() {
+        let mut encoder = Encoder::new();
+        (-30000i16).encode(&mut encoder).unwrap();
+        assert_eq!(vec![2, 0, 0, 0, 0xD0, 0x8A], pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_i32() {
+        let mut encoder = Encoder::new();
+        (-2000000000i32).encode(&mut encoder).unwrap();
+        assert_eq!(vec![4, 0, 0, 0, 0x00, 0x6C, 0xCA, 0x88],
+                   pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_i64() {
+        let mut encoder = Encoder::new();
+        (-9000000000000000000i64).encode(&mut encoder).unwrap();
+        assert_eq!(vec![8, 0, 0, 0, 0x00, 0x00, 0x7c, 0x1d, 0xaf, 0x93, 0x19, 0x83],
+                   pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_f32() {
+        let mut encoder = Encoder::new();
+        (1005.75f32).encode(&mut encoder).unwrap();
+        assert_eq!(vec![4, 0, 0, 0, 0x00, 0x70, 0x7b, 0x44],
+                   pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_f64() {
+        let mut encoder = Encoder::new();
+        (1005.75f64).encode(&mut encoder).unwrap();
+        assert_eq!(vec![8, 0, 0, 0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x6e, 0x8f, 0x40],
+                   pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_bool() {
+        let mut encoder = Encoder::new();
+        true.encode(&mut encoder).unwrap();
+        assert_eq!(vec![1, 0, 0, 0, 1], pull_data(&encoder));
+        let mut encoder = Encoder::new();
+        false.encode(&mut encoder).unwrap();
+        assert_eq!(vec![1, 0, 0, 0, 0], pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_string() {
+        let mut encoder = Encoder::new();
+        "".encode(&mut encoder).unwrap();
+        assert_eq!(vec![0, 0, 0, 0], pull_data(&encoder));
+        let mut encoder = Encoder::new();
+        "Hello, World!".encode(&mut encoder).unwrap();
+        assert_eq!(vec![13, 0, 0, 0, 72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33],
+                   pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_array() {
+        let mut encoder = Encoder::new();
+        [7i16, 1025, 33, 57].encode(&mut encoder).unwrap();
+        assert_eq!(vec![28, 0, 0, 0, 4, 0, 0, 0, 2, 0, 0, 0, 7, 0, 2, 0, 0, 0, 1, 4, 2, 0, 0, 0,
+                        33, 0, 2, 0, 0, 0, 57, 0],
+                   pull_data(&encoder));
+    }
+
+    #[test]
+    fn writes_tuple() {
+        let mut encoder = Encoder::new();
+        (2050i16, true, 7u8, "ABC012", vec![true, false, false, true])
+            .encode(&mut encoder)
+            .unwrap();
+        assert_eq!(vec![54, 0, 0, 0, 2, 0, 0, 0, 2, 8, 1, 0, 0, 0, 1, 1, 0, 0, 0, 7, 6, 0, 0, 0,
+                        65, 66, 67, 48, 49, 50, 24, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0,
+                        0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+                   pull_data(&encoder));
+    }
+
+    #[derive(RustcEncodable)]
+    struct TestStructOne {
+        a: i16,
+        b: bool,
+        c: u8,
+        d: String,
+        e: Vec<bool>,
+    }
+
+    #[test]
+    fn writes_simple_struct() {
+        let mut encoder = Encoder::new();
+        TestStructOne {
+                a: 2050i16,
+                b: true,
+                c: 7u8,
+                d: String::from("ABC012"),
+                e: vec![true, false, false, true],
+            }
+            .encode(&mut encoder)
+            .unwrap();
+        assert_eq!(vec![54, 0, 0, 0, 2, 0, 0, 0, 2, 8, 1, 0, 0, 0, 1, 1, 0, 0, 0, 7, 6, 0, 0, 0,
+                        65, 66, 67, 48, 49, 50, 24, 0, 0, 0, 4, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0,
+                        0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+                   pull_data(&encoder));
+    }
+
+    #[derive(RustcEncodable)]
+    struct TestStructPart {
+        a: String,
+        b: bool,
+    }
+
+    #[derive(RustcEncodable)]
+    struct TestStructBig {
+        a: Vec<TestStructPart>,
+        b: String,
+    }
+
+    #[test]
+    fn writes_complex_struct() {
+        let mut encoder = Encoder::new();
+        let mut parts = Vec::new();
+        parts.push(TestStructPart {
+            a: String::from("ABC"),
+            b: true,
+        });
+        parts.push(TestStructPart {
+            a: String::from("1!!!!"),
+            b: true,
+        });
+        parts.push(TestStructPart {
+            a: String::from("234b"),
+            b: false,
+        });
+        TestStructBig {
+                a: parts,
+                b: String::from("EEe"),
+            }
+            .encode(&mut encoder)
+            .unwrap();
+        assert_eq!(vec![66, 0, 0, 0, 55, 0, 0, 0, 3, 0, 0, 0, 12, 0, 0, 0, 3, 0, 0, 0, 65, 66,
+                        67, 1, 0, 0, 0, 1, 14, 0, 0, 0, 5, 0, 0, 0, 49, 33, 33, 33, 33, 1, 0, 0,
+                        0, 1, 13, 0, 0, 0, 4, 0, 0, 0, 50, 51, 52, 98, 1, 0, 0, 0, 0, 3, 0, 0, 0,
+                        69, 69, 101],
+                   pull_data(&encoder));
+        assert_eq!(70, encoder.len());
     }
 }
