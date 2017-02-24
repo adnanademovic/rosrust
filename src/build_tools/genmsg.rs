79710716fa4d4bcbd5197ef5b15c553b664ecd52
Adnan Ademovic <adnanademovic100@gmail.com>
2017-02-24 17:50:13 +0100

    Restructure message file parsing

--- src/build_tools/genmsg.rs
+++ src/build_tools/genmsg.rs
@@ -93,53 +93,32 @@ fn match_const_numeric(data: &str) -> Option<(FieldLine, String)> {
           captures.get(3).unwrap().as_str().into()))
 }
 
-fn match_line(data: &str) -> Result<Option<FieldInfo>, ()> {
+fn match_line(data: &str) -> Option<Result<FieldInfo, ()>> {
     if let Some((info, data)) = match_const_string(data.trim()) {
-        return Ok(Some(FieldInfo {
-            datatype: info.field_type,
-            name: info.field_name,
-            case: FieldCase::Const(data),
-        }));
+        return Some(FieldInfo::new(&info.field_type, &info.field_name, FieldCase::Const(data)));
     }
     let data = data.splitn(2, '#').next().unwrap().trim();
     if data == "" {
-        return Ok(None);
+        return None;
     }
     if let Some(info) = match_field(data) {
-        return Ok(Some(FieldInfo {
-            datatype: info.field_type,
-            name: info.field_name,
-            case: FieldCase::Unit,
-        }));
+        return Some(FieldInfo::new(&info.field_type, &info.field_name, FieldCase::Unit));
     }
     if let Some(info) = match_vector_field(data) {
-        return Ok(Some(FieldInfo {
-            datatype: info.field_type,
-            name: info.field_name,
-            case: FieldCase::Vector,
-        }));
+        return Some(FieldInfo::new(&info.field_type, &info.field_name, FieldCase::Vector));
     }
     if let Some((info, count)) = match_array_field(data) {
-        return Ok(Some(FieldInfo {
-            datatype: info.field_type,
-            name: info.field_name,
-            case: FieldCase::Array(count),
-        }));
+        return Some(FieldInfo::new(&info.field_type, &info.field_name, FieldCase::Array(count)));
     }
     if let Some((info, data)) = match_const_numeric(data) {
-        return Ok(Some(FieldInfo {
-            datatype: info.field_type,
-            name: info.field_name,
-            case: FieldCase::Const(data),
-        }));
+        return Some(FieldInfo::new(&info.field_type, &info.field_name, FieldCase::Const(data)));
     }
-    Err(())
+    Some(Err(()))
 }
 
 #[inline]
 fn match_lines(data: &str) -> Result<Vec<FieldInfo>, ()> {
-    let lines: Result<Vec<Option<_>>, ()> = data.split('\n').map(match_line).collect();
-    Ok(lines?.into_iter().filter_map(|v| v).collect())
+    data.split('\n').filter_map(match_line).collect()
 }
 
 #[derive(Debug,PartialEq)]
@@ -156,29 +135,21 @@ enum FieldCase {
     Const(String),
 }
 
-#[derive(Clone,Debug,PartialEq)]
-enum MemberCase {
-    Unit,
-    Vector,
-    Array(usize),
-}
-
-impl<'a> From<&'a FieldCase> for MemberCase {
-    fn from(case: &'a FieldCase) -> MemberCase {
-        match *case {
-            FieldCase::Unit => MemberCase::Unit,
-            FieldCase::Vector => MemberCase::Vector,
-            FieldCase::Array(v) => MemberCase::Array(v),
-            _ => panic!("Conversion not supported"),
-        }
-    }
-}
-
 #[derive(Debug,PartialEq)]
 struct FieldInfo {
-    datatype: String,
+    datatype: DataType,
     name: String,
     case: FieldCase,
+}
+
+impl FieldInfo {
+    fn new(datatype: &str, name: &str, case: FieldCase) -> Result<FieldInfo, ()> {
+        Ok(FieldInfo {
+            datatype: parse_datatype(&datatype).ok_or(())?,
+            name: name.to_owned(),
+            case: case,
+        })
+    }
 }
 
 #[derive(Debug,PartialEq)]
@@ -201,33 +172,14 @@ enum DataType {
     RemoteStruct(String, String),
 }
 
-#[derive(Debug,PartialEq)]
-struct MemberInfo {
-    datatype: DataType,
-    name: String,
-    case: MemberCase,
-}
-
-fn parse_members(fields: &[FieldInfo]) -> Result<Vec<MemberInfo>, ()> {
-    fields.iter()
-        .map(|v| {
-            Ok(MemberInfo {
-                datatype: parse_datatype(&v.datatype).ok_or(())?,
-                name: v.name.clone(),
-                case: MemberCase::from(&v.case),
-            })
-        })
-        .collect()
-}
-
 fn parse_datatype(datatype: &str) -> Option<DataType> {
     match datatype {
         "bool" => Some(DataType::Bool),
-        "int8" => Some(DataType::I8),
+        "int8" | "byte" => Some(DataType::I8),
         "int16" => Some(DataType::I16),
         "int32" => Some(DataType::I32),
         "int64" => Some(DataType::I64),
-        "uint8" => Some(DataType::U8),
+        "uint8" | "char" => Some(DataType::U8),
         "uint16" => Some(DataType::U16),
         "uint32" => Some(DataType::U32),
         "uint64" => Some(DataType::U64),
@@ -304,12 +256,12 @@ mod tests {
 
     #[test]
     fn match_line_works_on_legal_data() {
-        assert!(match_line("#just a comment").unwrap().is_none());
-        assert!(match_line("#  YOLO !   ").unwrap().is_none());
-        assert!(match_line("      ").unwrap().is_none());
+        assert!(match_line("#just a comment").is_none());
+        assert!(match_line("#  YOLO !   ").is_none());
+        assert!(match_line("      ").is_none());
 
         assert_eq!(FieldInfo {
-                       datatype: "geom_msgs/Twist".into(),
+                       datatype: DataType::RemoteStruct("geom_msgs".into(), "Twist".into()),
                        name: "myname".into(),
                        case: FieldCase::Unit,
                    },
@@ -318,26 +270,26 @@ mod tests {
                        .unwrap());
 
         assert_eq!(FieldInfo {
-                       datatype: "geom_msgs/Twist".into(),
+                       datatype: DataType::RemoteStruct("geom_msgs".into(), "Twist".into()),
                        name: "myname".into(),
                        case: FieldCase::Vector,
                    },
                    match_line("  geom_msgs/Twist [  ]   myname  # ...").unwrap().unwrap());
 
         assert_eq!(FieldInfo {
-                       datatype: "geom_msgs/Twist".into(),
+                       datatype: DataType::U8,
                        name: "myname".into(),
                        case: FieldCase::Array(127),
                    },
-                   match_line("  geom_msgs/Twist   [   127 ]   myname# comment").unwrap().unwrap());
+                   match_line("  char   [   127 ]   myname# comment").unwrap().unwrap());
         assert_eq!(FieldInfo {
-                       datatype: "string".into(),
+                       datatype: DataType::String,
                        name: "myname".into(),
                        case: FieldCase::Const("this is # data".into()),
                    },
                    match_line("  string  myname =   this is # data  ").unwrap().unwrap());
         assert_eq!(FieldInfo {
-                       datatype: "geom_msgs/Twist".into(),
+                       datatype: DataType::RemoteStruct("geom_msgs".into(), "Twist".into()),
                        name: "myname".into(),
                        case: FieldCase::Const("-444".into()),
                    },
@@ -348,31 +300,14 @@ mod tests {
     fn match_lines_parses_real_message() {
         let data = match_lines(include_str!("TwistWithCovariance.msg")).unwrap();
         assert_eq!(vec![FieldInfo {
-                            datatype: "Twist".into(),
+                            datatype: DataType::LocalStruct("Twist".into()),
                             name: "twist".into(),
                             case: FieldCase::Unit,
                         },
                         FieldInfo {
-                            datatype: "float64".into(),
-                            name: "covariance".into(),
-                            case: FieldCase::Array(36),
-                        }],
-                   data);
-    }
-
-    #[test]
-    fn parse_members_parses_real_message() {
-        let data = parse_members(&match_lines(include_str!("TwistWithCovariance.msg")).unwrap())
-            .unwrap();
-        assert_eq!(vec![MemberInfo {
-                            datatype: DataType::LocalStruct("Twist".into()),
-                            name: "twist".into(),
-                            case: MemberCase::Unit,
-                        },
-                        MemberInfo {
                             datatype: DataType::F64,
                             name: "covariance".into(),
-                            case: MemberCase::Array(36),
+                            case: FieldCase::Array(36),
                         }],
                    data);
     }
