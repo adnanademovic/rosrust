e813dc690410d76dac7a3ae32bf08656e41a735e
Adnan Ademovic <adnanademovic100@gmail.com>
2017-05-06 23:38:27 +0200

    Append share folder to cmake paths

--- src/build_tools/mod.rs
+++ src/build_tools/mod.rs
@@ -27,17 +27,31 @@ macro_rules! rosmsg_include {
 }
 
 pub fn depend_on_messages(messages: &[&str]) {
-    let variables = ["CMAKE_PREFIX_PATH", "ROSRUST_MSG_PATH"]
-        .iter()
-        .filter_map(|v| env::var(v).ok())
+    let cmake_paths = env::var("CMAKE_PREFIX_PATH")
+        .unwrap_or(String::new())
+        .split(":")
+        .filter_map(append_share_folder)
         .collect::<Vec<String>>();
-    let paths = variables
+    let extra_paths = env::var("ROSRUST_MSG_PATH")
+        .unwrap_or(String::new())
+        .split(":")
+        .map(String::from)
+        .collect::<Vec<String>>();
+    let paths = cmake_paths
         .iter()
-        .flat_map(|v| v.split(":"))
+        .chain(extra_paths.iter())
+        .map(|v| v.as_str())
         .collect::<Vec<&str>>();
     let output = genmsg::depend_on_messages(paths.as_slice(), messages).unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("msg.rs");
     let mut f = File::create(&dest_path).unwrap();
     write!(f, "{}", output).unwrap();
+}
+
+fn append_share_folder(path: &str) -> Option<String> {
+    Path::new(path)
+        .join("share")
+        .to_str()
+        .map(|v| v.to_owned())
 }
