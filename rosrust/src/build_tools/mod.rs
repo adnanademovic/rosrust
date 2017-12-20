400260bfe36f41320ae67e1e4d6782fed38bf23f
Adnan Ademovic <adnanademovic100@gmail.com>
2017-12-20 12:00:17 +0100

    Fix missing message files

--- rosrust/src/build_tools/mod.rs
+++ rosrust/src/build_tools/mod.rs
@@ -32,6 +32,11 @@ pub fn depend_on_messages(messages: &[&str]) {
         .split(":")
         .filter_map(append_share_folder)
         .collect::<Vec<String>>();
+    let cmake_alt_paths = env::var("CMAKE_PREFIX_PATH")
+        .unwrap_or(String::new())
+        .split(":")
+        .filter_map(append_src_folder)
+        .collect::<Vec<String>>();
     let extra_paths = env::var("ROSRUST_MSG_PATH")
         .unwrap_or(String::new())
         .split(":")
@@ -39,6 +44,7 @@ pub fn depend_on_messages(messages: &[&str]) {
         .collect::<Vec<String>>();
     let paths = cmake_paths
         .iter()
+        .chain(cmake_alt_paths.iter())
         .chain(extra_paths.iter())
         .map(|v| v.as_str())
         .collect::<Vec<&str>>();
@@ -51,4 +57,10 @@ pub fn depend_on_messages(messages: &[&str]) {
 
 fn append_share_folder(path: &str) -> Option<String> {
     Path::new(path).join("share").to_str().map(|v| v.to_owned())
+}
+
+fn append_src_folder(path: &str) -> Option<String> {
+    Path::new(path).join("..").join("src").to_str().map(|v| {
+        v.to_owned()
+    })
 }
