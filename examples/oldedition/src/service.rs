bb40ef21c75b98cb91a8b65b9b88493393344358
Adnan Ademovic <adnanademovic100@gmail.com>
2019-03-04 21:42:15 +0100

    Keep old edition example for compilation checks

---
+++ examples/oldedition/src/service.rs
@@ -0,0 +1,31 @@
+#![deny(warnings)]
+
+extern crate env_logger;
+#[macro_use]
+extern crate rosrust;
+
+mod msg;
+
+fn main() {
+    env_logger::init();
+
+    // Initialize node
+    rosrust::init("add_two_ints_server");
+
+    // Create service
+    // The service is stopped when the returned object is destroyed
+    let _service_raii =
+        rosrust::service::<msg::roscpp_tutorials::TwoInts, _>("add_two_ints", move |req| {
+            // Callback for handling requests
+            let sum = req.a + req.b;
+
+            // Log each request
+            ros_info!("{} + {} = {}", req.a, req.b, sum);
+
+            Ok(msg::roscpp_tutorials::TwoIntsRes { sum })
+        })
+        .unwrap();
+
+    // Block the thread until a shutdown signal is received
+    rosrust::spin();
+}
