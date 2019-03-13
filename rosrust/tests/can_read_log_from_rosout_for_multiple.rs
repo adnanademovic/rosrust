5afaf919a0a66a861e036db75c6c61f3e8dc96da
Adnan Ademovic <adnanademovic100@gmail.com>
2019-03-13 17:44:28 +0100

    Create failing test for multiple rosout publishers

--- rosrust/tests/can_read_log_from_rosout_for_multiple.rs
+++ rosrust/tests/can_read_log_from_rosout_for_multiple.rs
@@ -10,7 +10,7 @@ mod msg {
 }
 
 #[test]
-fn can_read_log_from_rosout_for_rosrust() {
+fn can_read_log_from_rosout_for_multiple() {
     let _roscore = util::run_roscore_for(util::Language::Multi, util::Feature::Log);
     let _publisher_cpp = util::ChildProcessTerminator::spawn(
         Command::new("rosrun")
@@ -36,28 +36,43 @@ fn can_read_log_from_rosout_for_rosrust() {
     rosrust::init("rosout_agg_listener");
 
     let (tx, rx) = unbounded();
+    let tx_agg = tx.clone();
 
     let _subscriber =
+        rosrust::subscribe::<msg::rosgraph_msgs::Log, _>("/rosout", 100, move |data| {
+            tx.send((false, data.level, data.name, data.msg)).unwrap();
+        })
+        .unwrap();
+
+    let _subscriber_agg =
         rosrust::subscribe::<msg::rosgraph_msgs::Log, _>("/rosout_agg", 100, move |data| {
-            tx.send((data.level, data.name, data.msg)).unwrap();
+            tx_agg
+                .send((true, data.level, data.name, data.msg))
+                .unwrap();
         })
         .unwrap();
 
     let mut rate = rosrust::rate(1.0);
 
     let mut expected_messages = BTreeSet::new();
-    expected_messages.insert("/talker_cpp");
-    expected_messages.insert("/talker_py");
-    expected_messages.insert("/talker_rust");
+    expected_messages.insert((true, "/talker_cpp".to_owned()));
+    expected_messages.insert((false, "/talker_cpp".to_owned()));
+    expected_messages.insert((true, "/talker_py".to_owned()));
+    expected_messages.insert((false, "/talker_py".to_owned()));
+    expected_messages.insert((true, "/talker_rust".to_owned()));
+    expected_messages.insert((false, "/talker_rust".to_owned()));
 
     for _ in 0..10 {
-        for (level, name, message) in rx.try_iter() {
+        for (aggregated, level, name, message) in rx.try_iter() {
             println!(
-                "Received message from {} at level {}: {}",
-                name, level, message
+                "Received {}message from {} at level {}: {}",
+                if aggregated { "aggregated " } else { "" },
+                name,
+                level,
+                message,
             );
             if level == 2 && message.contains("hello world") {
-                expected_messages.remove(name.as_str());
+                expected_messages.remove(&(aggregated, name));
             }
         }
         if expected_messages.is_empty() {
@@ -66,5 +81,20 @@ fn can_read_log_from_rosout_for_rosrust() {
         rate.sleep();
     }
 
-    panic!("Failed to receive data on /rosout_agg");
+    let cases = expected_messages
+        .iter()
+        .map(|(aggregated, name)| {
+            format!(
+                "{} on {}",
+                name,
+                if *aggregated {
+                    "/rosout_agg"
+                } else {
+                    "/rosout"
+                },
+            )
+        })
+        .collect::<Vec<String>>();
+
+    panic!("Failed to receive data from: {:?}", cases);
 }
