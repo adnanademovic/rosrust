df09d7a4df0eaeaee14f29f0d6735f657ea6d26e
Adnan Ademovic <adnanademovic100@gmail.com>
2019-03-29 19:13:19 +0100

    Add pubsub node that talks to itself

---
+++ examples/pubsub/src/loopback.rs
@@ -0,0 +1,47 @@
+#![deny(warnings)]
+
+use env_logger;
+use rosrust;
+
+mod msg;
+
+fn main() {
+    env_logger::init();
+
+    // Initialize node
+    rosrust::init("talker_listener");
+
+    // Create subscriber
+    // The subscriber is stopped when the returned object is destroyed
+    let _subscriber_raii = rosrust::subscribe("chatter", 200, |v: msg::std_msgs::String| {
+        // Callback for handling received messages
+        rosrust::ros_info!("Received: {}", v.data);
+    })
+    .unwrap();
+
+    // Create publisher
+    let chatter_pub = rosrust::publish("chatter", 200).unwrap();
+
+    let mut count = 0;
+
+    // Create object that maintains 10Hz between sleep requests
+    let mut rate = rosrust::rate(10.0);
+
+    // Breaks when a shutdown signal is sent
+    while rosrust::is_ok() {
+        // Create string message
+        let mut msg = msg::std_msgs::String::default();
+        msg.data = format!("hello world from rosrust {}", count);
+
+        // Log event
+        rosrust::ros_info!("Publishing: {}", msg.data);
+
+        // Send string message to topic via publisher
+        chatter_pub.send(msg).unwrap();
+
+        // Sleep to maintain 10Hz rate
+        rate.sleep();
+
+        count += 1;
+    }
+}
