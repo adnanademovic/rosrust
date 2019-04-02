e28c6ae6813c23f8c4b3c02caf1197593d60e76a
Adnan Ademovic <adnanademovic100@gmail.com>
2019-04-02 22:52:34 +0200

    Add relayed publisher/subscriber test

--- rosrust/tests/publisher_to_relayed_subscriber.rs
+++ rosrust/tests/publisher_to_relayed_subscriber.rs
@@ -9,20 +9,27 @@ mod msg {
 }
 
 #[test]
-fn publisher_to_inline_subscriber() {
-    let _roscore = util::run_roscore_for(util::Language::None, util::Feature::Publisher);
+fn publisher_to_relayed_subscriber() {
+    let _roscore = util::run_roscore_for(util::Language::Shell, util::Feature::Publisher);
+    let _subscriber = util::ChildProcessTerminator::spawn(
+        Command::new("rosrun")
+            .arg("topic_tools")
+            .arg("relay")
+            .arg("/chatter_pub")
+            .arg("/chatter_sub"),
+    );
 
     rosrust::init("hello_world_talker_listener");
 
     let (tx, rx) = unbounded();
 
     let _log_subscriber =
-        rosrust::subscribe::<msg::std_msgs::String, _>("chatter", 100, move |data| {
+        rosrust::subscribe::<msg::std_msgs::String, _>("chatter_sub", 100, move |data| {
             tx.send((2, data.data)).unwrap();
         })
         .unwrap();
 
-    let publisher = rosrust::publish::<msg::std_msgs::String>("chatter", 100).unwrap();
+    let publisher = rosrust::publish::<msg::std_msgs::String>("chatter_pub", 100).unwrap();
 
     let mut message = msg::std_msgs::String::default();
     message.data = "hello world".to_owned();
