b9d1ae7143a841811bb205909e1736aa25b75c50
Marco Esposito <marco.esposito@tum.de>
2019-05-19 12:51:10 +0200

    Better error message when .msg file not found

--- rosrust_codegen/src/error.rs
+++ rosrust_codegen/src/error.rs
@@ -4,4 +4,11 @@ error_chain! {
     foreign_links {
         Regex(::regex::Error);
     }
+
+    errors {
+        MessageNotFound(msg: String, folders: String) {
+            description("message not found in provided directories")
+            display("message {} not found in provided directories\nDirectories:\n{}", msg, folders)
+        }
+    }
 }
